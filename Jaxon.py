1ec37cd4845add4e3acbbc66f7b6196b6f5044af
jcaps-ops <jaxon.castro@ucas-edu.net>
2026-01-27 17:50:05 +0000

    Made the featureds for finding the values of the items and how to replace them

--- Jaxon.py
+++ Jaxon.py
@@ -12,14 +12,39 @@ No:
 
 
 """
+
+
 #Example dictonary Items_Dictonaties = {"Weapon":[#item name,#Slot type.#class required,#Weight,],"Armor":[],"Inventory":{} }
-Items_Dictonaties = {"Weapon":["Sword"],"Armor":["Iron armor"],"Inventory":{}}
+Items_Dictonaties = {"Weapon":["Sword","Weapon","none"],"Armor":["Iron armor","Armor"],"Inventory":["daggers","Weapon","None","staff","Weapon","Rouge"]}
 
 def inventory_management(Items_Dictonaties):
     print(f"Your charaters weapon is a {Items_Dictonaties["Weapon"][0]}")
     print(f"Your charaters is wearing {Items_Dictonaties["Armor"][0]} ")
-    print(f"In your inventory is {Items_Dictonaties["Inventory"]}")
-    
+    print(f"This is your inventory:")
+    val = 0
+    for x in Items_Dictonaties["Inventory"]:
+        val += 1
+        if val == 1:
+            print(x)
+        if val == 3:
+            val = 0
+    Player_answer = input("Would you like to Edit you inventory(1.Yes 2.No)")
+    if Player_answer == "1" or Player_answer == "yes":
+        answering = True
+        while answering:
+            Edit_item = input("What item in your inventory do you want to edit")
+            if Edit_item in Items_Dictonaties["Inventory"]:
+                answering = False
+                Item_index = Items_Dictonaties["Inventory"][Edit_item]
+                Item_slot = Items_Dictonaties["Inventory"][Item_index + 1]
+                Item_class = Items_Dictonaties["Inventory"][Item_index + 2]
+                Items_Dictonaties[Item_slot] = [Edit_item,Item_slot,Item_class]
+                
+                
+
+
+        
+
 
 
 inventory_management(Items_Dictonaties)
