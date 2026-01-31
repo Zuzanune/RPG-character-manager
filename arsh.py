bc425ae701ecdffaa764dfc16f08573f42abc5da
Zuzanune <Zane.carter@UCAS-edu.net>
2026-01-31 19:19:01 +0000

    changed some small things for ease of use

--- arsh.py
+++ arsh.py
@@ -6,6 +6,3 @@ import random
 # intro
 def intro():
     print("\nWelcome to your RPG Character Manager\nView and modify all your characters' information, skills and attributes\nTo choose what you want to do, type the corresponding number on the menus\n")
-
-# Database is now centralized in main.py to avoid duplication
-database = {}
